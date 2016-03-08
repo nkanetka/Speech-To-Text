08b1f1130d50c5df3e4655a9e843d1f74f0cef70
Nehal Kanetkar <nkanetkar@pelmorex.com>
2016-03-07 22:20:07 -0500

    Clean up main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import speech_recognition as sr
 
-WAV_FILE = "CYYZ-Twr-Mar-07-2016-2230Z_SMALL.wav"
-
-# print(WAV_FILE)
-
+WAV_FILE = "Audio.wav"
 r = sr.Recognizer()
 
-with sr.WavFile(WAV_FILE) as source:
-	# print(source)
-	audio = r.record(source)
-	# print(audio)
-r.recognize_google(audio)
-# print(r.recognize_sphinx(audio))
+def main():
+	with sr.WavFile(WAV_FILE) as source:
+		audio = r.record(source)
+
+	# print(r.recognize_google(audio))
+	print(r.recognize_sphinx(audio))
+
+if __name__ == "__main__":
+	main()
